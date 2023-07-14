ac1b871597c0a1952b91eb2d81fc915f4fb4b276
akashmony01 <akashmonya88@gmail.com>
2023-07-14 15:21:34 +0600

    first commit

---
+++ renderer/pages/home.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Head from 'next/head';
+import Link from 'next/link';
+
+function Home() {
+  return (
+    <React.Fragment>
+      <Head>
+        <title>Home - welcome to custom calculator</title>
+      </Head>
+      <section>
+        <div>
+          ghjkl
+        </div>
+      </section>
+    </React.Fragment>
+  );
+};
+
+export default Home;
