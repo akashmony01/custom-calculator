12037c2ce282764c442f203197fe361d3090cec3
git-jahid <jahid80820@gmail.com>
2023-07-21 00:46:24 +0600

    Schema Fix

--- prisma/seed/admins.js
+++ prisma/seed/admins.js
@@ -1,24 +1,8 @@
 const admins = [
     {
-        username: "akash",
+        username: "admin",
         password: "12345" // 12345
     },
-    {
-        username: "jahid",
-        password: "12345" // 12345
-    },
-    {
-        username: "shadhin",
-        password: "12345" // 12345
-    },
-    {
-        username: "prity",
-        password: "12345" // 12345
-    },
-    {
-        username: "saiful",
-        password: "12345" // 12345
-    }
 ];
 
 module.exports = admins;
